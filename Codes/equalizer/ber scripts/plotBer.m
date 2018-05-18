0471313a1f7269fb3bd9c66c06d9db2a8d81a479
Felipe <fbs@poli.ufrj.br>
2018-05-18 02:28:44 -0300

    Plot scripts added

--- Codes/equalizer/ber scripts/plotBer.m
+++ Codes/equalizer/ber scripts/plotBer.m
@@ -17,48 +17,122 @@ fileVector = [1 5 13 9];
 
 
 
-for i = 4:5
-    
-    figure
-    
-    for j = 1:length(fileVector)
+% for i = 4:5
+%     
+%     figure
+%     
+%     for j = 1:length(fileVector)
+% 
+%         if fileVector(j) < 10
+%             load (['resultsBER0' num2str(fileVector(j)) '.mat']);
+%         else
+%             load (['resultsBER' num2str(fileVector(j)) '.mat']);
+%         end
+% 
+%         semilogy(SNR,squeeze(ber(i,:)))
+%         hold on;
+%     end
+%     H = legend('V-PNLMS','VSM-PNLMS','V-RLS','VM-BEACON');
+%     set(H,'location','SouthWest');
+%     set(H,'interpreter','latex')
+%     
+%     xlabel('SNR [dB]','interpreter','latex');
+%     ylim([1e-6 1e0])
+%     xlim([0 30]);
+%     
+%     set(gca,'xtick',SNR);
+%   
+%     if i == 4
+%         ylabel('BER','interpreter','latex');
+%     else
+%         set(gca,'ytick',[]);
+%     end
+%         
+%     
+% %     formatFig( gcf ,['.' filesep 'figs' filesep '2017-07-12' filesep 'berFF'  num2str(i)],'en' , figProp );
+%     
+% end
+% 
+% close all;
+% fileVector = [3 7 15 11];
+% 
+% figure
+% 
+% for i = 5:5
+%     for k = 1:1
+%     
+%         for j = 1:length(fileVector)
+%             
+%             if fileVector(j) > 9
+%                 load (['resultsBER' num2str(fileVector(j)) '.mat']);
+%             else
+%                 load (['resultsBER0' num2str(fileVector(j)) '.mat']);
+%             end
+% 
+%             semilogy(SNR,squeeze(ber(i,k,:)))
+%             hold on;
+%         end
+%         H = legend('V-PNLMS','VSM-PNLMS','V-RLS','VM-BEACON');
+%         set(H,'location','SouthWest');
+%         set(H,'interpreter','latex')
+% 
+%         xlabel('SNR [dB]','interpreter','latex');
+%         ylim([1e-6 1e0])
+%         xlim([0 30]);
+% 
+%         set(gca,'xtick',SNR);
+% 
+%         ylabel('BER','interpreter','latex');
+%         
+%     end
+%     
+% %     formatFig( gcf ,['.' filesep 'figs' filesep '2017-07-12' filesep 'berDFE'],'en' , figProp );
+%     
+% end
 
-        if fileVector(j) < 10
-            load (['resultsBER0' num2str(fileVector(j)) '.mat']);
-        else
-            load (['resultsBER' num2str(fileVector(j)) '.mat']);
+
+close all;
+fileVector = [17 19];
+
+figure
+
+for i = 3:3
+    
+        for j = 1:length(fileVector)
+            
+            if fileVector(j) > 9
+                load (['resultsBER' num2str(fileVector(j)) '.mat']);
+            else
+                load (['resultsBER0' num2str(fileVector(j)) '.mat']);
+            end
+
+            semilogy(SNR,squeeze(ber(i,:)))
+            hold on;
         end
+        H = legend('V-PNLMS','VSM-PNLMS','V-RLS','VM-BEACON');
+        set(H,'location','SouthWest');
+        set(H,'interpreter','latex')
 
-        semilogy(SNR,squeeze(ber(i,:)))
-        hold on;
-    end
-    H = legend('V-PNLMS','VSM-PNLMS','V-RLS','VM-BEACON');
-    set(H,'location','SouthWest');
-    set(H,'interpreter','latex')
-    
-    xlabel('SNR [dB]','interpreter','latex');
-    ylim([1e-6 1e0])
-    xlim([0 30]);
-    
-    set(gca,'xtick',SNR);
-  
-    if i == 4
+        xlabel('SNR [dB]','interpreter','latex');
+        ylim([1e-6 1e0])
+        xlim([0 30]);
+
+        set(gca,'xtick',SNR);
+
         ylabel('BER','interpreter','latex');
-    else
-        set(gca,'ytick',[]);
-    end
         
     
-%     formatFig( gcf ,['.' filesep 'figs' filesep '2017-07-12' filesep 'berFF'  num2str(i)],'en' , figProp );
+    
+%     formatFig( gcf ,['.' filesep 'figs' filesep '2017-07-12' filesep 'berDFE'],'en' , figProp );
     
 end
 
 close all;
-fileVector = [3 7 15 11];
+fileVector = [18 20];
 
 figure
 
-for i = 5:5
+for i = 3:3
     for k = 1:1
     
         for j = 1:length(fileVector)
@@ -86,7 +160,7 @@ for i = 5:5
         
     end
     
-    formatFig( gcf ,['.' filesep 'figs' filesep '2017-07-12' filesep 'berDFE'],'en' , figProp );
+%     formatFig( gcf ,['.' filesep 'figs' filesep '2017-07-12' filesep 'berDFE'],'en' , figProp );
     
 end
 
